44a0861128da9762f9ea402b7b4c45be02e325d1
LeonamMiiller <leonam.miiller@gmail.com>
2023-05-04 21:45:33 -0300

    Just begun

---
+++ UseCase/Intf.Persistence.pas
@@ -0,0 +1,21 @@
+unit Intf.Persistence;
+
+interface
+
+uses
+  Entity.Saque, Entity.Pessoa, Entity.ContaBancaria;
+
+type
+  IPersistence = interface
+    procedure SalvarPessoa(aPessoa : TPessoa);
+    procedure CarregarContaBancaria(aContaBancaria : TContaBancaria);
+    procedure SalvarContaBancaria(aContaBancaria : TContaBancaria);
+    procedure SalvarSaque(aContaBancaria: TContaBancaria);
+    function ObterSaldo: double;
+    function CarregarPessoa(aIDPessoa : Integer) : TPessoa;
+
+  end;
+
+implementation
+
+end.
